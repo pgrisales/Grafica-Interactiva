116a50d776d2a5a84bbf6c440d6fe27124131782
batman <pgrisales@unal.edu.co>
2023-05-22 12:27:22 -0500

    map added

---
+++ mapa.js
@@ -0,0 +1,23 @@
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
+function frontera() {
+	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function un() {
+	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
+	document.getElementById("Info").scrollIntoView();
+}
+
+function parque() {
+	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
+	document.getElementById("Info").scrollIntoView();
+}
+function peru() {
+	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
+	document.getElementById("Info").scrollIntoView();
+}
+function brasil() {
+	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
+	document.getElementById("Info").scrollIntoView();
+}
