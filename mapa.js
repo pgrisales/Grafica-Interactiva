882f8cd788cca74696bb9e4acee3878db1f0c85f
batman <pgrisales@unal.edu.co>
2023-05-27 11:32:46 -0500

    mapa interactivo

--- mapa.js
+++ mapa.js
@@ -1,23 +1,30 @@
-//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
 function frontera() {
-	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
-	document.getElementById("Info").scrollIntoView();
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/tresfronteras.jpg" alt="frontera"><br><span>Frontera con Brasil</span><br><br>Frontera entre Colombia y Brasil, es abierta y se puede cruzar a pie, en carro o moto sin ningun problema.';
+    document.getElementById("Info").scrollIntoView();
 }
 
 function un() {
-	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
-	document.getElementById("Info").scrollIntoView();
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/unlet.jpg" alt="UN"><br><span>Universidad Nacional sede Amazonas</span><br><br>La Universidad Nacional de Colombia sede Amazonía, está ubicada frente al aeropuerto Internacional Alfredo Vásquez Cobo, en las afueras de la ciudad de Leticia.';
+    document.getElementById("Info").scrollIntoView();
 }
 
 function parque() {
-	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
-	document.getElementById("Info").scrollIntoView();
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/parque_santander.jpeg" alt="parque"><br><span>Parque Santander</span><br><br>Es el parque más importante de Leticia, y al igual que todo el Amazonas es un amplio pulmón verde en el que se puede apreciar una hermosa fuente en el centro del parque, una iglesia a la cual puedes ingresar y subir hasta la torre para tener una mejor vista y en la tarde apreciar el arribo y canto de miles de loritos que se mezclan en los árboles.';
+    document.getElementById("Info").scrollIntoView();
 }
 function peru() {
-	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
-	document.getElementById("Info").scrollIntoView();
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/santarosa.jpg" alt="peru"><br><span>Peru</span><br><br>Santa Rosa del Yavarí es una localidad peruana del distrito del Yavarí, ubicado de la provincia de Mariscal Ramón Castilla en el departamento de Loreto. Se puede ir en lancha desde Leticia o Tabatinga en unos 10 minutos la entrada es libre no se necesita ningun permiso.';
+    document.getElementById("Info").scrollIntoView();
 }
 function brasil() {
-	document.getElementById("Info").innerHTML = '<img class="imgM" src="images/foto08.jpg" alt="punto uno"><br><span>Entrada calle 26</span><br><br>Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto, texto, texto y mucho mas texto. Texto, texto y mucho mas texto. Texto, texto y texto, texto, texto, texto y mucho mas texto. Texto, texto y mas texto. Texto, texto y mas texto. Texto, texto y mas texto.<br> Texto, texto y mas texto. Texto, texto y mas texto.';
-	document.getElementById("Info").scrollIntoView();
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/tabatinga.jpg" alt="brasil"><br><span>Tabatinga</span><br><br>Tabatinga es una ciudad brasileña al extremo oeste del estado de Amazonas a orillas del río Amazonas. Al occidente de esta se encuentra con la triple frontera Brasil-Colombia-Perú.';
+    document.getElementById("Info").scrollIntoView();
+}
+function leti() {
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/leti.jpg" alt="leti"><br><span>Leticia</span><br><br>Leticia es un municipio fronterizo colombiano, es la capital del departamento del Amazonas. Se encuentra localizado en el extremo sur del país.';
+    document.getElementById("Info").scrollIntoView();
+}
+function rio() {
+    document.getElementById("Info").innerHTML = '<img class="imgM" src="imgs/rio.jpeg" alt="rio"><br><span>Rio Amazonas</span><br><br>El río Amazonas (también llamado Paraná-Uassú o Paranaguasú por tribus tupí-guaraní1​) es un río de América del Sur que atraviesa Perú (nacimiento), Colombia y Brasil (desembocadura). Es el río más largo y caudaloso del mundo.';
+    document.getElementById("Info").scrollIntoView();
 }
